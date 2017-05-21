7001e7d61a5cd41a201327e409d0eb7898f494cf
Marius Andra <marius.andra@gmail.com>
2017-05-21 20:01:48 +0200

    digest assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = false
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
