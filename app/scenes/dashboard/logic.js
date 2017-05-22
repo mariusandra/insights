d6ca997d66289d55de850c4e2b065b46c5ac27d5
Marius Andra <marius.andra@gmail.com>
2017-05-22 12:47:13 +0200

    remove temp

--- app/scenes/dashboard/logic.js
+++ app/scenes/dashboard/logic.js
@@ -1,42 +1,6 @@
 import Logic, { initLogic } from 'kea/logic'
 import { PropTypes } from 'react'
 
-// const outerActions = selectActionsFromLogic([
-//   otherScene, [
-//     'doSomething'
-//   ]
-// ])
-
-var defaultLayout = [
-  {
-    w: 1,
-    h: 1,
-    x: 0,
-    y: 0,
-    i: '0',
-    moved: false,
-    'static': false
-  },
-  {
-    w: 1,
-    h: 1,
-    x: 0,
-    y: 1,
-    i: '1',
-    moved: false,
-    'static': false
-  },
-  {
-    w: 4,
-    h: 4,
-    x: 2,
-    y: 0,
-    i: '2',
-    moved: false,
-    'static': false
-  }
-]
-
 @initLogic
 export default class DashboardLogic extends Logic {
   path = () => ['scenes', 'dashboard', 'index']
