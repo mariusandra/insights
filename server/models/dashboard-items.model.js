697bc784fe3fd34b1b310fba9d63638a37941307
Marius Andra <marius.andra@gmail.com>
2017-06-14 12:23:13 +0200

    lint

--- server/models/dashboard-items.model.js
+++ server/models/dashboard-items.model.js
@@ -1,12 +1,12 @@
-const NeDB = require('nedb');
-const path = require('path');
+const NeDB = require('nedb')
+const path = require('path')
 
 module.exports = function (app) {
-  const dbPath = app.get('nedb');
+  const dbPath = app.get('nedb')
   const Model = new NeDB({
     filename: path.join(dbPath, 'dashboard-items.db'),
     autoload: true
-  });
+  })
 
-  return Model;
-};
+  return Model
+}
--- server/services/dashboard-items/dashboard-items.service.js
+++ server/services/dashboard-items/dashboard-items.service.js
@@ -1,29 +1,29 @@
 // Initializes the `dashboard-items` service on path `/api/dashboard-items`
-const createService = require('feathers-nedb');
-const createModel = require('../../models/dashboard-items.model');
-const hooks = require('./dashboard-items.hooks');
-const filters = require('./dashboard-items.filters');
+const createService = require('feathers-nedb')
+const createModel = require('../../models/dashboard-items.model')
+const hooks = require('./dashboard-items.hooks')
+const filters = require('./dashboard-items.filters')
 
 module.exports = function () {
-  const app = this;
-  const Model = createModel(app);
-  const paginate = app.get('paginate');
+  const app = this
+  const Model = createModel(app)
+  const paginate = app.get('paginate')
 
   const options = {
     name: 'dashboard-items',
     Model,
     paginate
-  };
+  }
 
   // Initialize our service with any options it requires
-  app.use('/api/dashboard-items', createService(options));
+  app.use('/api/dashboard-items', createService(options))
 
   // Get our initialized service so that we can register hooks and filters
-  const service = app.service('api/dashboard-items');
+  const service = app.service('api/dashboard-items')
 
-  service.hooks(hooks);
+  service.hooks(hooks)
 
   if (service.filter) {
-    service.filter(filters);
+    service.filter(filters)
   }
-};
+}
