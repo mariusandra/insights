40370070f7e6c67c68e3a827ab1f7e3c4004e7e9
Marius Andra <marius.andra@gmail.com>
2017-06-15 17:00:00 +0200

    try with proces.cwd

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ const authentication = require('./authentication')
 const app = feathers()
 
 // Load app configuration
-app.configure(configuration(path.join(__dirname, '..')))
+app.configure(configuration(path.join(process.cwd(), '..')))
 
 // Enable CORS, security, compression, favicon and body parsing
 app.use(cors())
