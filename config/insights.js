875ce6e52d49320f6e445c542ae5ceeb79b55c10
Marius Andra <marius.andra@gmail.com>
2017-06-15 16:12:50 +0200

    config updates

--- config/insights.js
+++ config/insights.js
@@ -1,5 +1,11 @@
 module.exports = {
+  // connection string to your DB. only postgresql for now
   database: 'postgresql://localhost/insights_demo',
+
+  // timezone for the dates
   timezone: 'Europe/Brussels',
-  config: '../insights_demo/config/insights.yml'
+
+  // either a link to the insights.yml file or null to autodetect
+  // config: '../insights_demo/config/insights.yml'
+  config: null
 }
