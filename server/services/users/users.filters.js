e0ec6464a0b4374bb9727c0cb24bf73d508095b0
Marius Andra <marius.andra@gmail.com>
2017-06-16 16:44:14 +0200

    remove filter warnings

--- server/services/users/users.filters.js
+++ server/services/users/users.filters.js
@@ -1,5 +1,5 @@
 /* eslint no-console: 1 */
-console.warn('You are using the default filter for the users service. For more information about event filters see https://docs.feathersjs.com/api/events.html#event-filtering') // eslint-disable-line no-console
+// console.warn('You are using the default filter for the users service. For more information about event filters see https://docs.feathersjs.com/api/events.html#event-filtering') // eslint-disable-line no-console
 
 module.exports = function (data, connection, hook) { // eslint-disable-line no-unused-vars
   return data
