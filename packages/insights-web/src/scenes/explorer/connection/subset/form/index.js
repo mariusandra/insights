f6a04465ccffcd2cf98a98e0547eaff70d7cfde5
Marius Andra <marius.andra@gmail.com>
2020-02-01 10:11:44 +0100

    remove unused var

--- packages/insights-web/src/scenes/explorer/connection/subset/form/index.js
+++ packages/insights-web/src/scenes/explorer/connection/subset/form/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Form, Input, Modal, List, Tree, Checkbox } from 'antd'
+import { Button, Form, Input, Modal, Tree, Checkbox } from 'antd'
 import { useActions, useValues } from 'kea'
 
 import connectionsLogic from '../../logic'
