c750ee88f3a0fb40866643e42ef350a0e09b67a9
Marius Andra <marius.andra@gmail.com>
2020-02-03 21:06:57 +0100

    help text and subset options

--- packages/insights-web/src/scenes/explorer/connection/subset/form/index.js
+++ packages/insights-web/src/scenes/explorer/connection/subset/form/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Form, Input, Modal } from 'antd'
+import { Button, Divider, Form, Modal, Alert, Input, Select } from 'antd'
 import { useActions, useValues } from 'kea'
 
 import connectionsLogic from '../../logic'
@@ -16,7 +16,7 @@ function SubsetForm ({ form: { getFieldDecorator, validateFieldsAndScroll, getFi
     <Modal
       destroyOnClose
       visible={isSubsetOpen}
-      title='Edit Subset'
+      title='Configure "All Data"'
       onCancel={closeSubset}
       canOutsideClickClose
       width='85%'
@@ -29,19 +29,56 @@ function SubsetForm ({ form: { getFieldDecorator, validateFieldsAndScroll, getFi
         </Button>,
       ]}
     >
+      <Alert
+        message='What is the "All Data" Subset?'
+        description={
+          <span>
+            Insights uses "Subsets" to limit what can be seen in the explorer. Think of them as a partial views of your database.
+            Each subset can be shared with different teams in your organisation.
+            The "All Data" subset is what other subsets are based on. Changes here will be mirrored to all other subsets.
+          </span>
+        }
+        type="info"
+        showIcon
+      />
+      <Divider />
+
       <Form labelCol={{ span: 5 }} wrapperCol={{ span: 19 }} onSubmit={handleAdd}>
         <Form.Item label='Name'>
           {getFieldDecorator('keyword', {
-            initialValue: '',
+            initialValue: 'All Data',
             rules: [
               {
                 required: true,
                 message: 'Please input a name!',
               }
             ]
-          })(<Input placeholder='All Data' style={{width: '100%'}} />)}
+          })(<Input placeholder='All Data' disabled style={{width: '100%'}} />)}
+        </Form.Item>
+
+        <Form.Item label='New Models' extra='What to do with models that are added in the future'>
+          {getFieldDecorator('new_models', {
+            initialValue: 'add'
+          })(
+            <Select style={{width: '100%'}}>
+              <Select.Option value="add">Add automatically</Select.Option>
+              <Select.Option value="none">Never add automatically</Select.Option>
+            </Select>)}
+        </Form.Item>
+
+        <Form.Item label='New Fields' extra='What to do with fields in selected models'>
+          {getFieldDecorator('new_fields', {
+            initialValue: 'add'
+          })(
+            <Select style={{width: '100%'}}>
+              <Select.Option value="add">Add automatically to all selected models</Select.Option>
+              <Select.Option value="auto">Add automatically to only fully selected models</Select.Option>
+              <Select.Option value="none">Never add automatically</Select.Option>
+            </Select>)}
         </Form.Item>
       </Form>
+
+      <Divider />
 
       <h3>Select models and fields to include in this subset</h3>
       <Models />
